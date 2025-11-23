5e5cece4b87ca4c08a0767ebe08923f4bc673888
abdullah894 <125894281+abdullah894@users.noreply.github.com>
2025-11-23 11:57:37 +0500

    error fixed

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const backendUrl = process.env.VITE_BACKEND_URL;
+const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 axios.defaults.baseURL = backendUrl;
 
 export const AuthContext = createContext();
