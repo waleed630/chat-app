4563aa42652d55b0ad713999ce72e2b5dcefdd38
Waleed <m.waleedbinlatif@gmail.com>
2026-05-01 14:03:16 +0500

    fixed connection to mongodb in server.js

--- server/server.js
+++ server/server.js
@@ -45,14 +45,23 @@ app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
+// //connect to MongoDb
+// await connectDb();
+
+// if (process.env.NODE_ENV !== "production") {
+//   const PORT = process.env.PORT || 5000;
+
+//   server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
+// }
+
+// //Export server for vercel
+// export default server;
+
 //connect to MongoDb
 await connectDb();
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-
-  server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
-}
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
 
 //Export server for vercel
 export default server;
